33913c5543e3c8c3c87f0c9920ec4ad602769401
Alexey Dudnik <alexey.dudnik@hys-enterprise.com>
2018-09-24 08:55:21 +0300

    [Phase1: C11] Added ProductionRuleSelectorAction WPF project. Implemented ImplicationRuleSelectorAction panel.

---
+++ FuzzyPortfolioManagement/assemblies/UI/WPF/ProductionRuleSelectorAction/Panels/ImplicationRuleSelectorAction.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace ProductionRuleSelectorAction.Panels
+{
+    public partial class ImplicationRuleSelectorAction : Window
+    {
+        public ImplicationRuleSelectorAction()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BrowseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                DefaultExt = ".txt",
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            bool? fileDialogResult = openFileDialog.ShowDialog();
+
+            if (fileDialogResult == true)
+            {
+                FilePathTextBox.Text = openFileDialog.FileName;
+                ImplicationRulesTextBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+    }
+}
