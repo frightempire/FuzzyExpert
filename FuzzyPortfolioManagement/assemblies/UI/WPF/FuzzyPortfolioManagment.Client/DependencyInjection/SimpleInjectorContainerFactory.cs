18bca243a699ecc8a9c4d7b0f689e40ee70cf64e
Alexey Dudnik <alexey.dudnik@hys-enterprise.com>
2018-09-28 10:08:55 +0300

    [Phase1: C14] Added DI Container factory and resolver to FuzzyPortfolioManagment.Client.

---
+++ FuzzyPortfolioManagement/assemblies/UI/WPF/FuzzyPortfolioManagment.Client/DependencyInjection/SimpleInjectorContainerFactory.cs
@@ -0,0 +1,30 @@
+﻿using CommonLogic.Implementations;
+using CommonLogic.Interfaces;
+using ProductionRuleManager.Implementations;
+using ProductionRuleManager.Interfaces;
+using ProductionRulesParser.Implementations;
+using ProductionRulesParser.Interfaces;
+using SimpleInjector;
+
+namespace FuzzyPortfolioManagment.Client.DependencyInjection
+{
+    public class SimpleInjectorContainerFactory
+    {
+        private readonly Container _container = new Container();
+
+        public Container CreateSimpleInjectorContainer()
+        {
+            _container.RegisterInstance(typeof(IImplicationRulePreProcessor), new ImplicationRulePreProcessor());
+            _container.RegisterInstance(typeof(IImplicationRuleParser), new ImplicationRuleParser());
+            _container.Register<IImplicationRuleCreator, ImplicationRuleCreator>();
+
+            _container.RegisterInstance(typeof(IFileReader), new FileReader());
+            _container.Register<IImplicationRuleProvider, FileImplicationRuleProvider>();
+
+            _container.Register<IImplicationRuleManager, ImplicationRuleManager>(Lifestyle.Singleton);
+
+            _container.Verify();
+            return _container;
+        }
+    }
+}
---
+++ FuzzyPortfolioManagement/assemblies/UI/WPF/FuzzyPortfolioManagment.Client/DependencyInjection/SimpleInjectorResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CommonLogic;
+using SimpleInjector;
+
+namespace FuzzyPortfolioManagment.Client.DependencyInjection
+{
+    public class SimpleInjectorResolver
+    {
+        private readonly Container _simpleInjectorContainer;
+
+        public SimpleInjectorResolver(Container simpleInjectorContainer)
+        {
+            ExceptionAssert.IsNull(simpleInjectorContainer);
+
+            _simpleInjectorContainer = simpleInjectorContainer;
+        }
+
+        public object Resolve(Type itemType)
+        {
+            try
+            {
+                return _simpleInjectorContainer.GetInstance(itemType);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public void Dispose()
+        {
+            _simpleInjectorContainer.Dispose();
+        }
+    }
+}
